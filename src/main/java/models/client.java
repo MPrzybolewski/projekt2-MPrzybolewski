35254a9574b4ffd1ed52e3b193a57e3327b9c303
mprzybolewski <mprzybolewski@gmail.com>
2018-04-26 19:34:30 +0200

    add client model implementation

--- src/main/java/models/client.java
+++ src/main/java/models/client.java
@@ -1,4 +1,51 @@
 package models;
 
 public class client {
+    int id;
+    String firstName;
+    String secondName;
+    String email;
+
+    public client(int id, String firstName, String secondName, String email) {
+        this.id = id;
+        this.firstName = firstName;
+        this.secondName = secondName;
+        this.email = email;
+    }
+
+    public client(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
