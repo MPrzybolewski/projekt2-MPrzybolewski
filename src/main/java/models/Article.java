1405bbab0b03a1946cb1926ae6b174e6a0fc7d8f
mprzybolewski <mprzybolewski@gmail.com>
2018-04-26 21:06:59 +0200

    add missing getters and setters for Article model

--- src/main/java/models/Article.java
+++ src/main/java/models/Article.java
@@ -3,5 +3,35 @@ package models;
 public class Article {
     int id;
     String name;
-    double price;  
+    double price;
+
+    public Article(int id, String name, double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
