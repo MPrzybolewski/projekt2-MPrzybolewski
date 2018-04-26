204621d0eb026c6143d636cb71f291fb726180bc
mprzybolewski <mprzybolewski@gmail.com>
2018-04-26 19:35:32 +0200

    add detailOrder model implementation

--- src/main/java/models/detailOrder.java
+++ src/main/java/models/detailOrder.java
@@ -1,4 +1,27 @@
 package models;
 
 public class detailOrder {
+    int objectId;
+    int orderId;
+
+    public detailOrder(int objectId, int orderId) {
+        this.objectId = objectId;
+        this.orderId = orderId;
+    }
+
+    public int getObjectId() {
+        return objectId;
+    }
+
+    public void setObjectId(int objectId) {
+        this.objectId = objectId;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
 }
