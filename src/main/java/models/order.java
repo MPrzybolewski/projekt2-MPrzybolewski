fb1fc9104bfb4ec5376a92767a44e4d545d9c439
mprzybolewski <mprzybolewski@gmail.com>
2018-04-26 19:38:14 +0200

    add order model implementation

--- src/main/java/models/order.java
+++ src/main/java/models/order.java
@@ -1,4 +1,27 @@
 package models;
 
 public class order {
+    int id;
+    int clientId;
+
+    public order(int id, int clientId) {
+        this.id = id;
+        this.clientId = clientId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(int clientId) {
+        this.clientId = clientId;
+    }
 }
